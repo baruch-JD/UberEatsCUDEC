4a55dce7b919be8cfa3a329bf799f33b62f9035d
baruch-JD <baruchdduarte@gmail.com>
2026-06-17 21:18:31 -0600

    cambios

--- js/index.js
+++ js/index.js
@@ -1,13 +1,50 @@
- btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo')
+let contenido = '';
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  // nav menu
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
-  // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+// nav menu
+const menus = document.querySelectorAll('.side-menu');
+M.Sidenav.init(menus, {edge: 'right'});
+// add recipe form
+const forms = document.querySelectorAll('.side-form');
+M.Sidenav.init(forms, {edge: 'left'});
 });
 
 btnAgregarPlatillo.addEventListener('click', function(){
-alert('platillo agregado jsjsjs');
+alert('Platillo agregado')
 });
+
+function mostrarPlatillo(Platillo, id){
+contenido += `
+<div class = "card-panel recipe white row"
+id="${id}"> 
+<div class = "recipe-details">
+<div class = "recipe-title">
+${Platillo.nombre}
+</div>
+
+<div class = "recipe-ingredients">
+${Platillo.ingredientes}
+</div>
+
+<div class = "recipe-price">
+${Platillo.Precio}
+</div>
+
+
+</div>
+<div class = "recipe-delete">
+<i class = "material-icons"  data-id="${id}">delete_outline</i>
+
+</div>`;
+document.querySelector('.recipes').innerHTML = contenido;
+
+}
+
+function actualizarPlatillo(platillo, id){
+let tarjeta = document.getElementById(`${id}`);
+tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+    tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+tarjeta.querySelector(".recipe-price").innerHTML = platillo.Precio;
+
+}   
