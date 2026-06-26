48fc7cce2322ee7f389219f67856c94e19c3105b
baruch-JD <baruchdduarte@gmail.com>
2026-06-25 21:19:13 -0600

    cambios25/06/2026

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6fcMXd29_t2HmdVMNdU5QrikaUQ0dYNA",
+  authDomain: "ubereatsbaruch.firebaseapp.com",
+  projectId: "ubereatsbaruch",
+  storageBucket: "ubereatsbaruch.firebasestorage.app",
+  messagingSenderId: "177016882704",
+  appId: "1:177016882704:web:49295885b26d8decdc2c95",
+  measurementId: "G-8G9VLR3TJS"
+};
+
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
--- js/index.js
+++ js/index.js
@@ -5,10 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
 // nav menu
 const menus = document.querySelectorAll('.side-menu');
 M.Sidenav.init(menus, {edge: 'right'});
+
+
 // add recipe form
 const forms = document.querySelectorAll('.side-form');
 M.Sidenav.init(forms, {edge: 'left'});
 });
+
+
 
 btnAgregarPlatillo.addEventListener('click', function(){
 alert('Platillo agregado')
@@ -36,7 +40,9 @@ ${Platillo.Precio}
 <div class = "recipe-delete">
 <i class = "material-icons"  data-id="${id}">delete_outline</i>
 
+</div>
 </div>`;
+
 document.querySelector('.recipes').innerHTML = contenido;
 
 }
